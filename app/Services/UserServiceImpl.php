13322d5dfa4c8c6cd71d17f643c3d36bb970cad1
gmorikawa <morikawa.gabriel@outlook.jp>
2025-03-29 23:54:17 +0900

    Class separation per file fix

---
+++ app/Services/UserServiceImpl.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Exception;
+
+class UserServiceImpl implements UserService
+{
+    public function findAll()
+    {
+        return User::all();
+    }
+
+    public function findById(string $id)
+    {
+        return User::find($id);
+    }
+
+    public function findByUsername(string $username)
+    {
+        return User::where("username", $username)
+            ->first();
+    }
+
+    public function findByEmail(string $email)
+    {
+        return User::where("email", $email)
+            ->first();
+    }
+
+    public function create(User $user)
+    {
+        if (!$this->isUsernameUnique($user->username)) {
+            throw new Exception("Username is already registered.");
+        }
+
+        if (!$this->isEmailUnique($user->email)) {
+            throw new Exception("Email is already registered.");
+        }
+
+        $user->save();
+
+        return $user;
+    }
+
+    public function update(string $id, User $user)
+    {
+        if (!$this->isUsernameUnique($user->username)) {
+            throw new Exception("Username is already registered.");
+        }
+
+        if (!$this->isEmailUnique($user->email)) {
+            throw new Exception("Email is already registered.");
+        }
+
+        $entity = $this->findById($id);
+
+        if ($user) {
+            $entity->name = $user->name;
+            $entity->email = $user->email;
+            $entity->username = $user->username;
+        }
+
+        $entity->save();
+
+        return $entity;
+    }
+
+    public function remove(string $id)
+    {
+        return User::destroy($id);
+    }
+
+    private function isUsernameUnique(string $username, string $ignoreId = ""): bool
+    {
+        $user = $this->findByUsername($username);
+
+        return is_null($user) || (!is_null($user) && $user->id === $ignoreId);
+    }
+
+    private function isEmailUnique(string $email, string $ignoreId = ""): bool
+    {
+        $user = $this->findByEmail($email);
+
+        return is_null($user) || (!is_null($user) && $user->id === $ignoreId);
+    }
+}
